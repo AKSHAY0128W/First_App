d52c7c3704d1b6b85adc6111fb871c13d8df4df6
PANCHAL AKSHAY <97942661+AKSHAY0128W@users.noreply.github.com>
2022-09-14 20:48:54 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+double addNumber(double num1, double num2) {
+//   print(num1 + num2);
+  return num1 + num2;
+}
+
+void main() {
+  var result = addNumber(20, 20.5);
+  result = addNumber(5 , 5);
+   // result =("Hello!");
+   print(result + 2);
+}
